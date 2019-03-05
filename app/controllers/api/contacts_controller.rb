49d791bf43655644c653163f5cbda44ffcd8b58d
prestontewell <preston@tewellphoto.com>
2019-03-05 09:58:48 -0600

    update create action to RESTful and add update action

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -10,5 +10,27 @@ class Api::ContactsController < ApplicationController
     render 'show.json.jbuilder'
   end
 
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name], 
+      last_name: params[:last_name],
+      email: params[:email], 
+      phone_number: params[:phone_number]
+      )
+    @contact.save
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    contact_id = params[:id]
+    @contact = Contact.find_by(id:contact_id)
+    @contact.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+      )
+    render 'show.json.jbuilder'
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     get '/contacts' => 'contacts#index'
     get '/contacts/:id' => 'contacts#show'
+    post '/contacts' => 'contacts#create'
+    patch '/contacts/:id' => 'contacts#update'
   end
 end
