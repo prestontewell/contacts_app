3d114a36524142c0b2c247d7394a7a3cd00d44f1
prestontewell <preston@tewellphoto.com>
2019-03-07 19:31:38 -0600

    add email validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,7 @@ class Contact < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-
+  validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
   def run_created_at
     created_at.strftime("%e %b %Y %H:%M:%S%p")
   end
