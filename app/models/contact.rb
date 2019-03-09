2ea75b293c6904ba92d37aa7d81a24ea02184035
prestontewell <preston@tewellphoto.com>
2019-03-09 14:08:37 -0600

    add email format validation to Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,11 +3,12 @@ class Contact < ApplicationRecord
     "#{first_name} #{middle_name} #{last_name}"
   end
 
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
   def run_created_at
     created_at.strftime("%e %b %Y %H:%M:%S%p")
   end
